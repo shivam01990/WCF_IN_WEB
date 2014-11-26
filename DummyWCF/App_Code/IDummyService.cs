13434ad0ff0bbab5ab4ba3f93f2838a6dc52964a
shivam <shivam01990@gmail.com>
2014-11-27 01:02:12 +0530

    Dummy WCF Test

---
+++ DummyWCF/App_Code/IDummyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDummyService" in both code and config file together.
+[ServiceContract]
+public interface IDummyService
+{
+	[OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json,UriTemplate = "/DoWork/{id}/{time}")]
+	string DoWork(string id, string time);
+}
